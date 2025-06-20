e7b60eebd3393308d458fee9dbf9d770b2ce3d02
marme <marcel.1710@live.de>
2025-06-20 20:16:35 +0200

    handle component id change when LLM_Web_search is loaded

--- setup.js
+++ setup.js
@@ -10,7 +10,9 @@ var chatInputContainer = document.getElementById("chat-input-container");
 chatInputContainer.insertAdjacentElement("beforeend", document.querySelector('.progress-container-container'));
 document.querySelector('.progress-container-container').style.marginTop = "-15px";
 // Avoid dead space at the bottom of the chat tab
-document.getElementById("component-1").style.marginBottom = "-80px";
+var chatPageElem = document.getElementById("component-0");
+if (!chatPageElem) chatPageElem = document.getElementById("component-1")
+chatPageElem.style.marginBottom = "-80px";
 
 
 var notify_checkbox = document.getElementById("change_notify_checkbox").children[1].firstChild;
