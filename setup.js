0e8ba96ac8f3383b9ec297daaffde0f790c7fc66
marme <marcel.1710@live.de>
2026-02-23 15:26:22 +0100

    remove unused code in setup.js

--- setup.js
+++ setup.js
@@ -1,11 +1,3 @@
-var chatTabLabel = document.getElementById("chat-tab");
-var hiddenChatTabButton = document.getElementById("hidden-chat-tab-button");
-chatTabLabel.addEventListener('click', function() {
-  hiddenChatTabButton.click();
-});
-
-
-
 var chatInputContainer = document.getElementById("chat-input-container");
 chatInputContainer.insertAdjacentElement("beforeend", document.querySelector('.progress-container-container'));
 document.querySelector('.progress-container-container').style.marginTop = "-15px";
